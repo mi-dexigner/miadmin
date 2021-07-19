1557e228a228a6ff9b4e6cd8a175d10970982bd9
Muhammad <midexigner@gmail.com>
2021-07-20 00:13:12 +0500

    complete admin ui for react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Sidebar from './components/Sidebar/Sidebar'
 import Home from './pages/Home/Home'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import List from './pages/User/List';
+import UserEdit from './pages/User/UserEdit';
+import UserAdd from './pages/User/UserAdd';
+import ProductList from './pages/Products/List';
+import ProductEdit from './pages/Products/ProductEdit';
+import ProductAdd from './pages/Products/ProductAdd';
 const App = () => {
   return (
     <Router>
@@ -18,19 +23,19 @@ const App = () => {
            <List />
           </Route>
           <Route path="/user/:userId">
-           
+          <UserEdit />
           </Route>
           <Route path="/newUser">
-            
+          <UserAdd />
           </Route>
           <Route path="/products">
-            
+            <ProductList/>
           </Route>
           <Route path="/product/:productId">
-            
+            <ProductEdit/>
           </Route>
           <Route path="/newproduct">
-            
+            <ProductAdd/>
           </Route>
         </Switch>
       </div>
